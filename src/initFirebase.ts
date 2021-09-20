6cd8b36d6599d7cf7c910f42ce16da65641806f3
nelioyann <nanayannick1998@gmail.com>
2021-09-20 18:40:27 +0200

    update

--- src/initFirebase.ts
+++ src/initFirebase.ts
@@ -4,13 +4,20 @@ import "firebase/firestore"
 import "firebase/functions"
 
 
+// const config = {
+//     apiKey: process.env.REACT_APP_FIREBASE_CONFIG_API_KEY,
+//     projectId: process.env.REACT_APP_FIREBASE_CONFIG_PROJECT_ID,
+//     authDomain: process.env.REACT_APP_FIREBASE_CONFIG_AUTH_DOMAIN,
+// }
+
 const config = {
-    apiKey: process.env.REACT_APP_FIREBASE_CONFIG_API_KEY,
-    projectId: process.env.REACT_APP_FIREBASE_CONFIG_PROJECT_ID,
-    authDomain: process.env.REACT_APP_FIREBASE_CONFIG_AUTH_DOMAIN,
+    apiKey: "AIzaSyC78qJ3dKFn0MDwyirTPcK3isC9T_1yTdU",
+    authDomain: "grithub-5254d.firebaseapp.com",
+    projectId: "grithub-5254d",
+    storageBucket: "grithub-5254d.appspot.com",
+    messagingSenderId: "416816848828",
+    appId: "1:416816848828:web:e2750ef946f71ca59915b9"
 }
-
-
 
 // console.log(process.env.REACT_APP_FIREBASE_CONFIG_API_KEY)
 function initFirebase(){
