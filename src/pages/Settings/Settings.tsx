c01c686885f6dbb19ce3adc22ad79ec3bb95a918
version-bumper <bumper@boringday.co>
2021-10-10 16:27:23 +0000

    "Updated version 0.0.16 -> 0.0.17."

--- src/pages/Settings/Settings.tsx
+++ src/pages/Settings/Settings.tsx
@@ -167,7 +167,7 @@ const Settings: React.FC = () => {
                 <IonLabel>Attributions</IonLabel>
               </IonItem>
               <IonItem color="light" lines="none" button={true}>
-                <IonLabel>Version: v0.0.16 </IonLabel>
+                <IonLabel>Version: v0.0.17 </IonLabel>
               </IonItem>
             </div>
           </div>
